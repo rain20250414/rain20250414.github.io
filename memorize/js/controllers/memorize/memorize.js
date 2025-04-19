c26e2ebb4e58c05350616c4cefc061b33cafbabd
rainguo <rainguo@lexin.com>
2025-04-19 19:49:05 +0800

    增加merge对比插件，添加时间样式

--- memorize/js/controllers/memorize/memorize.js
+++ memorize/js/controllers/memorize/memorize.js
@@ -4,6 +4,7 @@ app.controller('memorizeController', function($scope, $http0, toaster, $modal) {
         pn: 1,
 
     };
+    $scope.now = new Date();
 
     $scope.reset = function() {
         $scope.params['search'] = '';
@@ -20,6 +21,7 @@ app.controller('memorizeController', function($scope, $http0, toaster, $modal) {
                 if(data.status) {
                     $scope.data = data.data;
                     $scope.params['pn'] = data.data.number + 1;
+                    $scope.now = new Date();
                 }
                 else {
                     toaster.pop('error', "error");
@@ -70,6 +72,10 @@ app.controller('memorizeController', function($scope, $http0, toaster, $modal) {
     $scope.test = function(record) {
         openTestForm(record);
     }
+
+    $scope.isPastDate = function(itemDate) {
+        return new Date(itemDate) < $scope.now;
+    };
 });
 app.controller('memorizeAddFormController', function($scope, $http0, toaster, items, $modal, $modalInstance) {
     $scope.save = function($event) {
@@ -93,33 +99,67 @@ app.controller('memorizeTestFormController', function($scope, $http0, toaster, i
     $scope.showText = items.content;
     $scope.showForm1 = true;
     $scope.showForm2 = false;
+    $scope.showCompare = false;
+
+    var comp;
+
+    setTimeout(() => {
+        comp = $('#compare');
+
+        comp.mergely({
+        cmsettings: {
+            readOnly: false,
+            lineWrapping: true
+        },
+        wrap_lines: true,
+
+        editor_width: 'calc(50% - 25px)',
+        editor_height: '100%'
+        
+        });
+        comp.mergely('lhs', $scope.showText);
+    }, 1000);
 
     $scope.start = function($event) {
         $scope.showForm1 = false;
         $scope.showForm2 = true;
     };
 
-    function submit(success) {
+    function upload($event, success) {
         $http0.post1('/record/test', {id: items.id, success: success}).then(function(data) {
             if(data.status) {
-                toaster.pop('success', "success");
-                $modalInstance.close();
+                if (success) {
+                    $modalInstance.close();
+                    toaster.pop('success', "success");
+                }
             }
             else {
                 toaster.pop('error', data.msg);
-                $($event.target).removeClass('active disabled');
             }
+            $($event.target).removeClass('active disabled');
         });
     }
 
     $scope.submit = function($event) {
         if (Object.is($scope.showText, $scope.inputText)) {
-            submit(true);
+            upload($event, true);
         } else {
-            toaster.pop('error', "failed");
+            comp.mergely('rhs', $scope.inputText);
+            $scope.showForm2 = false;
+            $scope.showCompare = true;
+            $($event.target).removeClass('active disabled');
         }
-        $($event.target).removeClass('active disabled');
     };
+
+    $scope.pass = function($event) {
+        upload($event, true);
+    }
+
+    $scope.reject = function($event) {
+        upload($event, false);
+        $scope.showForm2 = true;
+        $scope.showCompare = false;
+    }
 
     $scope.cancel = function() {
         $modalInstance.close();
